78a5e7f6aef65b810a4ab1864debdde06f23e8ce
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-07-30 22:50:00 -0300

    Feature - Upload of files

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,8 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
-const models = [User];
+import File from '../app/models/File';
+const models = [User, File];
 
 class Database {
   constructor() {
